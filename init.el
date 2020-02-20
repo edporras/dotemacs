b6beb413c050dbc90c3f287b57f2f2b41556b452
Ed Porras <github@digressed.net>
2020-02-20 12:09:11 +0100

    column-number-mode is already set in 08-appearance. New stuff to ignore

--- init.el
+++ init.el
@@ -16,7 +16,6 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(column-number-mode t)
  '(menu-bar-mode nil)
  '(package-selected-packages
    (quote
