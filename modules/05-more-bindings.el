b5f4bb825bd185b13c2566074f2e5fb4b2ef7628
Ed Porras <dev@digressed.net>
2020-10-29 13:28:22 +0100

    Add match-paren using %.

--- modules/05-more-bindings.el
+++ modules/05-more-bindings.el
@@ -81,4 +81,11 @@
 (global-set-key (kbd "C-<") 'mc/mark-previous-like-this)
 (global-set-key (kbd "C-c C-<") 'mc/mark-all-like-this)
 
+(defun match-paren (arg)
+  "Go to the matching paren if on a paren; otherwise insert %."
+  (interactive "p")
+  (cond ((looking-at "\\s(") (forward-list 1) (backward-char 1))
+        ((looking-at "\\s)") (forward-char 1) (backward-list 1))
+        (t (self-insert-command (or arg 1)))))
+
 ;;; 05-more-bindings.el ends here
