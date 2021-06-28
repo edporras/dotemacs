2f8f42d0fddb5cd29666657a25e8ae7f27a302ad
Ed Porras <dev@digressed.net>
2021-06-28 10:02:51 +0200

    eshell prompts

--- modules/03-utils.el
+++ modules/03-utils.el
@@ -7,6 +7,103 @@
   ;; (exec-path-from-shell-variables '("SSH_AUTH_SOCK" "SSH_AGENT_PID"))
   (exec-path-from-shell-initialize))
 
+;; from https://www.emacswiki.org/emacs/EshellPrompt
+(defun fish-path (path max-len)
+  "Return a potentially trimmed-down version of the directory PATH, replacing
+parent directories with their initial characters to try to get the character
+length of PATH (sans directory slashes) down to MAX-LEN."
+  (let* ((components (split-string (abbreviate-file-name path) "/"))
+         (len (+ (1- (length components))
+                 (reduce '+ components :key 'length)))
+         (str ""))
+    (while (and (> len max-len)
+                (cdr components))
+      (setq str (concat str
+                        (cond ((= 0 (length (car components))) "/")
+                              ((= 1 (length (car components)))
+                               (concat (car components) "/"))
+                              (t
+                               (if (string= "."
+                                            (string (elt (car components) 0)))
+                                   (concat (substring (car components) 0 2)
+                                           "/")
+                                 (string (elt (car components) 0) ?/)))))
+            len (- len (1- (length (car components))))
+            components (cdr components)))
+    (concat str (reduce (lambda (a b) (concat a "/" b)) components))))
+
+(defun with-face (str &rest face-plist)
+  (propertize str 'face face-plist))
+
+(defun shk-eshell-prompt ()
+  (let ((header-bg "#888"))
+    (concat
+     (with-face (concat (eshell/pwd) " ") :background header-bg)
+     (with-face (format-time-string "(%Y-%m-%d %H:%M) " (current-time)) :background header-bg :foreground "#888")
+     (with-face
+      (or (ignore-errors (format "(%s)" (vc-responsible-backend default-directory))) "")
+      :background header-bg)
+     (with-face "\n" :background header-bg)
+     (with-face user-login-name :foreground "blue")
+     "@"
+     (with-face "localhost" :foreground "green")
+     (if (= (user-uid) 0)
+         (with-face " #" :foreground "red")
+       " $")
+     " ")))
+
+(defun shortened-path (path max-len)
+  "Return a modified version of `path', replacing some components
+      with single characters starting from the left to try and get
+      the path down to `max-len'"
+  (let* ((components (split-string (abbreviate-file-name path) "/"))
+         (len (+ (1- (length components))
+                 (reduce '+ components :key 'length)))
+         (str ""))
+    (while (and (> len max-len)
+                (cdr components))
+      (setq str (concat str (if (= 0 (length (car components)))
+                                "/"
+                              (string (elt (car components) 0) ?/)))
+            len (- len (1- (length (car components))))
+            components (cdr components)))
+    (concat str (reduce (lambda (a b) (concat a "/" b)) components))))
+
+(defun rjs-eshell-prompt-function ()
+  (concat (fish-path (eshell/pwd) 40)
+          (if (= (user-uid) 0) " # " " $ ")))
+
+(setq eshell-prompt-function 'rjs-eshell-prompt-function)
+(setq eshell-highlight-prompt nil)
+
+(defun eshell-here ()
+  "Opens up a new shell in the directory associated with the
+    current buffer's file. The eshell is renamed to match that
+    directory to make multiple eshell windows easier."
+  (interactive)
+  (let* ((parent (if (buffer-file-name)
+                     (file-name-directory (buffer-file-name))
+                   default-directory))
+         (height (/ (window-total-height) 3))
+         (name   (car (last (split-string parent "/" t)))))
+    (split-window-vertically (- height))
+    (other-window 1)
+    (eshell "new")
+    (rename-buffer (concat "*eshell: " name "*"))
+
+    (insert (concat "ls"))
+    (eshell-send-input)))
+
+(global-set-key (kbd "C-!") 'eshell-here)
+
+(defun eshell/x ()
+  (insert "exit")
+  (eshell-send-input)
+  (delete-window))
+
+;; eshell ansi handling
+(add-hook 'shell-mode-hook 'ansi-color-for-comint-mode-on)
+
 ;; undo-tree is pretty nifty
 (require 'undo-tree)
 (global-undo-tree-mode 1)
@@ -20,9 +117,14 @@
 (global-set-key (kbd "C-x g") 'magit-status)
 (define-key magit-mode-map [remap previous-line] 'magit-previous-line)
 (define-key magit-mode-map [remap next-line] 'magit-next-line)
-; re-enable --set-upstream switch
-(magit-define-popup-switch 'magit-push-popup
-  ?u "Set upstream" "--set-upstream")
+(setq magit-push-current-set-remote-if-missing nil)
+;; re-enable --set-upstream switch
+;; (magit-define-popup-switch 'magit-push-popup
+;;                            ?u "Set upstream" "--set-upstream")
+
+;; change dired find-ls
+(require 'find-dired)
+(setq find-ls-option '("-print0 | xargs -0 ls -ld" . "-ld"))
 
 ;; docker
 (require 'dockerfile-mode)
